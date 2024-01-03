52745d59facb8f97bd04780cdd0c6c146c8301e2
Tamir Sida <tamirsida@Tamirs-MacBook-Pro.local>
2024-01-03 23:44:14 +0200

    hello

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -24,6 +24,74 @@ public class OneOfEachStats {
 		//// just like you had in the previous version, except that the 
 		//// randomization will be based on the given seed.
 		//// This is the only change that you have to do in the program.
-		    
+		/**
+ *  Computes some statistics about families in which the parents decide 
+ *  to have children until they have at least one child of each gender.
+ *  The program expects to get one command-line argument: an int value
+ * 	that determines how many families to simulate.
+ */
+
+		//// Put your code here
+		
+/**
+ *  Simulates the formation of a family in which the parents decide 
+ *  to have children until they have at least one child of each gender.
+ */
+		int num = Integer.parseInt(args[0]);
+		String answer = "";
+		boolean g = false;
+		boolean b = false;
+		int pick;
+		int countt= 0;
+		int avgkids= 0;
+		int twokids = 0;
+		int threekids= 0;
+		int fourmore= 0;
+		for (int i=0; i<num; i++){
+			g=false;
+			b=false;
+			countt=0;
+
+			do {
+				pick = (int) (Math.random()*2);
+				if (pick == 1) {
+					g = true;
+					
+					//answer += "g ";
+					}else {
+					b = true;	
+				}
+				countt++;
+			}
+				while (!b||!g);
+			
+				
+				avgkids+=countt;
+				if (countt==2){
+					twokids++;
+				}else if (countt==3){
+					threekids++;
+				}else if (countt>=4){
+					fourmore++;
+				
+				}
+			}
+					
+					System.out.println("Average: " + ((double) avgkids)/((double)num));
+					System.out.println("Number of families with 2 children: " + twokids);
+					System.out.println("Number of families with 3 children: " + threekids);
+					System.out.println("Number of families with 4 or more children: " + fourmore);
+
+				
+
+						}
+			
+
+
+
 	}
-}
+
+
+		    
+	
+
--- Reverse.java
+++ Reverse.java
@@ -5,5 +5,25 @@
 public class Reverse {
 	public static void main (String[] args){
 		//// Put your code here
+
+		String word= args[0];
+		int wrd_Length = word.length();
+		int middle_chr = 0;
+
+		if (wrd_Length==0){
+		System.out.println(" ");
 	}
+		else  {
+		// get the mid char
+		middle_chr = wrd_Length/2;
+	
+	for (int i = (word.length()-1); i>=0; i--){
+		System.out.print(word.charAt(i));
+	}
+		System.err.println();
+
+		System.out.println("The middle character is " + word.charAt(middle_chr));
+
+	}
+}
 }
