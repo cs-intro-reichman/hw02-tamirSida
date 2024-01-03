4ac31bbdc2eb69a517f580ec03818b4ed212360f
Tamir Sida <tamirsida@Tamirs-MacBook-Pro.local>
2024-01-04 00:01:42 +0200

    damka

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -7,9 +7,9 @@ public class DamkaBoard {
 		for (int i=0;i<n;i++) {
 			for (int j= 0; j<n; j++) {
 				if (i%2==0){
-				System.out.print(" * ");
+				System.out.print("* ");
 				} else { 
-					System.out.print("  *");
+					System.out.print(" *");
 				}
 			}
 			System.out.println();
