b3ad680da93a57e9013d9ad840b7989e2d89994e
Tamir Sida <tamirsida@Tamirs-MacBook-Pro.local>
2024-01-04 00:03:11 +0200

    out

--- Reverse.java
+++ Reverse.java
@@ -23,7 +23,7 @@ public class Reverse {
 	for (int i = (word.length()-1); i>=0; i--){
 		System.out.print(word.charAt(i));
 	}
-		System.err.println();
+		System.out.println();
 
 		System.out.println("The middle character is " + word.charAt(middle_chr));
 
